e2a1280562f522cf2d46c259575bf2526d51aa28
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-11-29 17:39:12 +0100

    Fase 1 Completada

--- src/RestaurantAPP.java
+++ src/RestaurantAPP.java
@@ -1,9 +1,24 @@
+import java.util.*;
 
 public class RestaurantAPP {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		//Variables necesarias
+		
+		final int NOTE5 = 5;
+		final int NOTE10 = 10;
+		final int NOTE20 = 20;
+		final int NOTE50 = 50;
+		final int NOTE100 = 100;
+		final int NOTE200 = 200;
+		final int NOTE500 = 500;
+		
+		int totalcost;
+		
+		ArrayList<String> menu = new ArrayList();
+		ArrayList<String> price = new ArrayList();
+				
 	}
 
 }
