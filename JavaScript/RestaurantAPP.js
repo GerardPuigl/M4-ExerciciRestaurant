87de7ad6329c69c53ba63d90dc18d505a6656e45
gerardpuiglatorre <gerardpuig31@hotmail.com>
2020-12-06 19:35:11 +0100

    Fase 1 JS Completada

---
+++ JavaScript/RestaurantAPP.js
@@ -0,0 +1,14 @@
+//Variables necessaries
+
+const NOTE5 = 5;
+const NOTE10 = 10;
+const NOTE20 = 20;
+const NOTE50 = 50;
+const NOTE100 = 100;
+const NOTE200 = 200;
+const NOTE500 = 500;
+
+var totalcost;
+
+var menu = new Array;
+var price = new Array;
